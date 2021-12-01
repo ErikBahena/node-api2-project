9f6e21d313c14f46b2ba0ce547dc2ee8c163d0f3
[Erik Bahena] <[erikjbahena@gmail.com]>
2021-11-30 21:09:08 -0800

    done with as much as I could Today

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,122 @@
 // implement your posts router here
+const express = require("express");
+const router = express.Router();
+
+const Model = require("./posts-model");
+
+// get all posts
+router.get("/", (req, res) => {
+  Model.find()
+    .then((posts) => res.status(200).json(posts))
+    .catch(() =>
+      res
+        .status(500)
+        .json({ message: "The posts information could not be retrieved" })
+    );
+});
+
+// get post by id
+router.get("/:id", (req, res) => {
+  Model.findById(req.params.id)
+    .then((post) => {
+      if (!post) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist" });
+        return;
+      }
+
+      res.status(200).json(post);
+    })
+    .catch(() =>
+      res
+        .status(500)
+        .json({ message: "The post information could not be retrieved" })
+    );
+});
+
+// create a new "post" lol.
+router.post("/", (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    res
+      .status(400)
+      .json({ message: "Please provide title and contents for the post" });
+    return;
+  }
+
+  Model.insert(req.body)
+    .then((post) => {
+      res.status(201).json(post);
+    })
+    .catch(() =>
+      res.status(500).json({
+        message: "There was an error while saving the post to the database",
+      })
+    );
+});
+// update a post
+router.put("/:id", (req, res) => {
+  if (!req.body.title || !req.body.contents) {
+    res
+      .status(400)
+      .json({ message: "Please provide title and contents for the post" });
+    return;
+  }
+
+  Model.update(req.params.id, req.body)
+    .then((post) => {
+      if (!post) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist" });
+        return;
+      }
+
+      res.status(200).json(post);
+    })
+    .catch(() =>
+      res
+        .status(500)
+        .json({ message: "The post information could not be modified" })
+    );
+});
+// delete a post
+router.delete("/:id", (req, res) => {
+  Model.remove(req.params.id)
+    .then((post) => {
+      if (!post) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist" });
+        return;
+      }
+
+      res.status(204);
+    })
+    .catch(() =>
+      res.status(500).json({ message: "The post could not be removed" })
+    );
+});
+
+// get comments on a post
+router.get("/:id/comments", (req, res) => {
+  Model.findPostComments(req.params.id)
+    .then((comments) => {
+      if (~comments) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist" });
+
+        return;
+      }
+
+      res.status(200).json(comments);
+    })
+    .catch(() =>
+      res
+        .status(500)
+        .json({ message: "The comments information could not be retrieved" })
+    );
+});
+
+module.exports = router;
